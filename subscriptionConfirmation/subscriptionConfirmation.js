e88d86fcb65acba423a1751179e0cca7b19c88af
Petre Cosmin Badescu <badescupetrecosmin@gmail.com>
2020-09-06 12:31:11 +0300

    'changes'

--- subscriptionConfirmation/subscriptionConfirmation.js
+++ subscriptionConfirmation/subscriptionConfirmation.js
@@ -1,4 +1,11 @@
 
 $(document).ready(function() {
-    confettiAnimation.start();
+    confetti({
+        particleCount: 180,
+        origin:{x:.63, y:.7},
+        startVelocity:45,
+        spread:60,
+        decay:.9,
+        ticks :400
+      });
 });
