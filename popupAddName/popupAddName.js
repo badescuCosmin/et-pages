4b58e4b20338bc1a791f89a244365e65957d3df6
Petre Cosmin Badescu <cosmin@luxon-tech.com>
2020-10-14 10:22:13 +0300

    'birthday'

--- popupAddName/popupAddName.js
+++ popupAddName/popupAddName.js
@@ -69,19 +69,7 @@ $(document).ready(function () {
     });
 
     function setInput() {
-        if(inputs.length > 2){
-            for(i=0; i< inputs.length/2 -1 ; i++) {
-                inputRestrictor.setInputOnlyWithDigits(`birthday${x}`);
-                inputRestrictor.setInputWithExactCharactersLength(`birthday${x}`, 10);
-                inputRestrictor.putCharacterAfterNumberOfCharacters(`birthday${x}`, "/", 2);
-                inputRestrictor.setDate(`birthday${x}`); 
-            }
-        }
-        inputRestrictor.setInputOnlyWithDigits("birthday");
-        inputRestrictor.setInputWithExactCharactersLength("birthday", 10);
-        inputRestrictor.putCharacterAfterNumberOfCharacters("birthday", "/", 2);
-        inputRestrictor.setDate("birthday");
-        // step1 inputs
+
         var inputsValidator = new FormValidator({ inputsList: inputs });
 
         $("#btn-cta").on("click", function () {
@@ -89,4 +77,37 @@ $(document).ready(function () {
         });
     };
     setInput();
+
+    
+    let input_birthday = document.getElementById('birthday');
+    let currentYear = new Date().getFullYear();
+
+    function checkBirthdayValue(str, max) {
+        if (str.charAt(0) !== '0' || str == '00') {
+            let num = parseInt(str);
+            if (isNaN(num) || num <= 0 || num > max) num = 1;
+            if (num > parseInt(max.toString().charAt(0)) && num.toString().length == 1) {
+                str = '0' + num;
+            } else {
+                str = num.toString()
+            }
+        };
+        return str;
+    };
+
+    input_birthday.addEventListener('input', function (e) {
+        this.type = 'text';
+        let input = this.value;
+        if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
+        let values = input.split('/').map(function (v) {
+            return v.replace(/\D/g, '')
+        });
+        if (values[0]) values[0] = checkBirthdayValue(values[0], 31); // day check
+        if (values[1]) values[1] = checkBirthdayValue(values[1], 12); // month check
+        if (values[1]) values[1] = checkBirthdayValue(values[1], currentYear); // year check
+        let output = values.map(function (v, i) {
+            return v.length == 2 && i < 2 ? v + ' / ' : v;
+        });
+        this.value = output.join('').substr(0, 14);
+    });
 });
