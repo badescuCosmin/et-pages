c27e5f9799cae39565cea591cd9789eef4bbddea
Petre Cosmin Badescu <cosmin@luxon-tech.com>
2020-10-16 17:31:49 +0300

    'changes'

--- popupAddName/popupAddName.js
+++ popupAddName/popupAddName.js
@@ -12,64 +12,16 @@ $(document).ready(function () {
         allowedCharacters: 'Alpha'
     },
     {
-        id: 'birthday-day',
+        id: 'date',
         fieldLabel: 'Birthday',
         required: true,
         type: 'text',
-        minLength: '2',
-        charactersLength: 2
+        minLength: '10',
+        charactersLength: 10
     },
-    {
-        id: 'birthday-mounth',
-        fieldLabel: 'Birthday',
-        required: true,
-        type: 'text',
-        minLength: '2',
-        charactersLength: 2
-    },
-    {
-        id: 'birthday-year',
-        fieldLabel: 'Birthday',
-        required: true,
-        type: 'text',
-        minLength: '4',
-        charactersLength: 4
-    }
+    
 ];
-    $('#birthday-label').on('click', function(){
-        document.getElementById("birthday-day").focus();  
-    })
-    $('#birthday-day').on('keyup', function(){
-        if(this.value.length === 2){
-            document.getElementById("birthday-mounth").focus();
-        }
-        if(this.value > 31){
-            this.value = '01'
-        }
-    })
-    $('#birthday-mounth').on('keyup', function(e){
-        if(this.value.length ===  2){
-            if(this.value > 12){
-                this.value = '01'
-            }
-            document.getElementById("birthday-year").focus();
-        }
-        if(e.keyCode === 8){
-           if(!this.value.length){
-            document.getElementById("birthday-day").focus();  
-           }
-        }
-    })
-    $('#birthday-year').on('keyup', function(e){
-        if(this.value.length  === 4){
-            document.getElementById("btn-cta").focus();
-        }
-        if(e.keyCode === 8){
-            if(!this.value.length){
-             document.getElementById("birthday-mounth").focus();  
-            }
-         }
-    })
+
     $(add_button).click(function (e) {
         e.preventDefault();
         if (x < max_fields) {
@@ -129,7 +81,56 @@ $(document).ready(function () {
         });
     };
     setInput();
+    var date = document.getElementById('date');
 
+    function checkValue(str, max) {
+      if (str.charAt(0) !== '0' || str == '00') {
+        var num = parseInt(str);
+        if (isNaN(num) || num <= 0 || num > max) num = 1;
+        str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
+      };
+      return str;
+    };
+    
+    date.addEventListener('input', function(e) {
+      this.type = 'text';
+      var input = this.value;
+      if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
+      var values = input.split('/').map(function(v) {
+        return v.replace(/\D/g, '')
+      });
+      if (values[0]) values[0] = checkValue(values[0], 12);
+      if (values[1]) values[1] = checkValue(values[1], 31);
+      var output = values.map(function(v, i) {
+        return v.length == 2 && i < 2 ? v + ' / ' : v;
+      });
+      this.value = output.join('').substr(0, 14);
+    });
+    
+    date.addEventListener('blur', function(e) {
+      this.type = 'text';
+      var input = this.value;
+      var values = input.split('/').map(function(v, i) {
+        return v.replace(/\D/g, '')
+      });
+      var output = '';
+      
+      if (values.length == 3) {
+        var year = values[2].length !== 4 ? parseInt(values[2]) + 2000 : parseInt(values[2]);
+        var month = parseInt(values[0]) - 1;
+        var day = parseInt(values[1]);
+        var d = new Date(year, month, day);
+        if (!isNaN(d)) {
+          document.getElementById('result').innerText = d.toString();
+          var dates = [d.getMonth() + 1, d.getDate(), d.getFullYear()];
+          output = dates.map(function(v) {
+            v = v.toString();
+            return v.length == 1 ? '0' + v : v;
+          }).join(' / ');
+        };
+      };
+      this.value = output;
+    });
 
     
 });
